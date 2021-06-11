f5f159067a12875def85581fa908b3f945ee16ef
Yasaswini-Jaladurgam <jyashjaladurgam@gmail.com>
2021-06-11 16:41:48 +0530

    Add files via upload

---
+++ redux-anecdotes/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import Notification from './components/Notification'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+const App = (props) => {
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => props.initializeAnecdotes(anecdotes))
+  }, []) // eslint-disable-line
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Filter />
+      {props.notification !== null &&
+      <Notification />}
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+export default connect(mapStateToProps, { initializeAnecdotes })(App)
